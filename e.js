6408c5c3ad557c97545f9c347f31cd7d857b8f7a
Anh Tran <anhtnt@elightup.com>
2023-06-09 09:41:03 +0700

    Support clicking on children elements to toggle and nested toggles

--- e.js
+++ e.js
@@ -1,7 +1,19 @@
-document.addEventListener( 'click', e => {
-	if ( ! e.target.matches( '[x-toggle]' ) ) {
-		return;
+{
+	document.addEventListener( 'click', e => loop( e.target, 'x-toggle', toggle ) );
+
+	// Run a callback for the element and its parents.
+	const loop = ( el, attribute, callback ) => {
+		do {
+			callback( el, attribute );
+			el = el.parentNode?.closest( `[${attribute}]` );
+		} while ( el );
 	}
-	const [ selector, className ] = e.target.getAttribute( 'x-toggle' ).split( ':' );
-	document.querySelector( selector ).classList.toggle( className );
-} );
+
+	const toggle = ( el, attribute ) => {
+		if ( ! el.matches( `[${attribute}]` ) ) {
+			return;
+		}
+		const [ selector, className ] = el.getAttribute( attribute ).split( ':' );
+		document.querySelector( selector ).classList.toggle( className );
+	}
+}
